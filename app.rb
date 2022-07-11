784534cef9ba97ab8bb06a231ed8a20d0c5d8c40
Paris Monson <parismonson@yahoo.com>
2022-07-11 15:59:20 +0100

    Server set up successfully

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Hello World!'
+  end
+
+  get '/hello' do
+    name = params[:name]
+    return "Hello #{name}"
+  end
+
+  post '/submit' do
+    name = params[:name] 
+    message = params[:message]
+    return "Thanks #{name}, you sent this message: #{message}"
+  end
+end
